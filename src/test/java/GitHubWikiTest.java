a8d9d520a484596c97e0a8a2f1ee8754788f8df9
Андрей <konstityciii@mail.ru>
2025-03-19 16:11:07 +0300

    Edit

---
+++ src/test/java/GitHubWikiTest.java
@@ -0,0 +1,30 @@
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubWikiTest {
+    @BeforeAll
+    static void setupConfig() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 5000; // default 4000
+    }
+    @Test
+    void checkSoftAssertionsPage() {
+        // Открыть страницу репозитория Selenide
+        open("https://github.com/selenide/selenide");
+
+        // Перейти в раздел Wiki проекта
+        $(byText("Wiki")).click();
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-content").shouldHave(text("Soft assertions"));
+        $("#wiki-content").$(byText("Soft assertions")).click();
+        $(byTagAndText("h4","3. Using JUnit5 extend test class:")).shouldBe(visible);
+    }
+}
